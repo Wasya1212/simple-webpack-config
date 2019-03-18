ace7af8c5e7d264190eadcfb0401088bd4feee15
wasya1212 <leonardo.1212@yandex.ua>
2019-03-18 02:00:42 +0200

    added simple frontpage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,87 @@
 import video from '../assets/videos/green_cell.mp4';
 
-// alert("hello world");
+import $ from 'jquery';
+import jQuery from 'jquery';
+
+import anime from 'animejs';
+
+import './libs/slick/slick/slick.js';
+import './libs/slick/slick/slick.css';
+
+$(document).ready(() => {
+  $('.partners-slider').slick({
+    arrows: false,
+    fade: false,
+    draggable: true,
+    dots: true,
+    slidesToShow: 4
+  });
+
+  $('.clients-slider').slick({
+    arrows: false,
+    fade: true,
+    draggable: false
+  });
+
+  $('.clients-names-slider li:first-child').addClass('slick-active');
+
+  $('.clients-names-slider li').click(function(e) {
+    $('.clients-slider').slick('slickGoTo', $('.clients-names-slider li').index(this));
+    $('.slick-active').removeClass('slick-active');
+    $(this).addClass('slick-active');
+  });
+
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true
+  });
+
+  hideAllTopSliderInfos();
+  showTopSliderInfo(0);
+
+  $('.slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    showTopSliderInfo();
+  });
+  $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    hideTopSliderInfo();
+  });
+});
+
+function showTopSliderInfo(duration = 1500) {
+  anime({
+    targets: '.slick-current .text a',
+    translateY: 0,
+    delay: 200,
+    duration: duration
+  });
+  anime({
+    targets: '.slick-current .text p',
+    translateY: 0,
+    delay: 150,
+    duration: duration
+  });
+  anime({
+    targets: '.slick-current .text h3',
+    translateY: 0,
+    delay: 100,
+    duration: duration
+  });
+}
+
+function hideTopSliderInfo() {
+  anime({
+    targets: ['.slick-slide .text p', '.slick-slide .text h3', '.slick-slide .text a'],
+    translateY: 500,
+    duration: 0
+  });
+}
+
+function hideAllTopSliderInfos() {
+  anime({
+    targets: ['.slick-slide .text p', '.slick-slide .text h3', '.slick-slide .text a'],
+    translateY: 500,
+    duration: 0
+  });
+
+}
